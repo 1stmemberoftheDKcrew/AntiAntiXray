35a736927d102b4067378baec119770069c8c355
1stmemberoftheDKcrew <48365836+1stmemberoftheDKcrew@users.noreply.github.com>
2021-03-09 21:05:12 -0500

    Update Config.java
    added stuff to make better on corejourney yes yes

--- src/main/java/me/constantindev/antiantixray/Etc/Config.java
+++ src/main/java/me/constantindev/antiantixray/Etc/Config.java
@@ -7,14 +7,14 @@ import org.lwjgl.glfw.GLFW;
 
 public class Config {
     public static int rad = 5;
-    public static long delay = 10;
+    public static long delay = 2;
     public static Manager manager = new Manager();
     public static boolean scanAll = false;
     public static boolean auto = false;
     public static int mtreshold = 5;
-    public static Block[] checkblocks = {Blocks.OBSIDIAN, Blocks.CLAY, Blocks.MOSSY_COBBLESTONE,
-            Blocks.DIAMOND_ORE, Blocks.REDSTONE_ORE, Blocks.IRON_ORE, Blocks.COAL_ORE, Blocks.LAPIS_ORE,
-            Blocks.GOLD_ORE, Blocks.EMERALD_ORE, Blocks.NETHER_GOLD_ORE, Blocks.NETHER_QUARTZ_ORE};
+    public static Block[] checkblocks = {
+            Blocks.DIAMOND_ORE, Blocks.LAPIS_ORE,
+            Blocks.ANCIENT_DEBRIS, Blocks.CRAFTING_TABLE, Blocks.NETHER_QUARTZ_ORE};
     public static int kcScan = GLFW.GLFW_KEY_Y;
     public static int kcRemove = GLFW.GLFW_KEY_V;
 
