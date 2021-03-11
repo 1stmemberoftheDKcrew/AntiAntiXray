bc23240ea8c928646f2dd4b77f9401a70913dd46
1stmemberoftheDKcrew <48365836+1stmemberoftheDKcrew@users.noreply.github.com>
2021-03-11 17:09:21 -0500

    Update Config.java

--- src/main/java/me/constantindev/antiantixray/Etc/Config.java
+++ src/main/java/me/constantindev/antiantixray/Etc/Config.java
@@ -12,7 +12,7 @@ public class Config {
     public static Manager manager = new Manager();
     public static boolean scanAll = false;
     public static boolean auto = false;
-    public static int mtreshold = 5;
+    public static int mtreshold = 2;
     public static Block[] checkblocks = {
             Blocks.DIAMOND_ORE, Blocks.LAPIS_ORE, Blocks.ANCIENT_DEBRIS, Blocks.CRAFTING_TABLE, Blocks.NETHER_QUARTZ_ORE, Blocks.EMERALD_ORE, Blocks.COAL_ORE,};
     public static int kcScan = GLFW.GLFW_KEY_Y;
